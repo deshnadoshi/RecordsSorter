dda7f69c0a509f5685fa1ebd7e5dbf8aeea3af42
deshnadoshi <deshna.doshi@gmail.com>
2024-01-29 15:14:43 -0500

    current record

--- record.js
+++ record.js
@@ -12,29 +12,28 @@ function process_input(){
             console.log("Please rename your file to input.txt and re-run the program."); 
         } else {
             let file_data = data.toString(); 
-            
+            let if_record_end = false;
+            let if_record_begin = false; 
+
             file_data.split(/\r?\n/).forEach(line =>  {
-                let if_record_end = false;
-                let if_record_begin = false; 
-                 // the record currently being read and stored in a file
-                // console.log("here1"); 
 
                 if ((line.toLowerCase()).localeCompare(begin_keyword) == 0){
                     if_record_begin = true;  
                 } 
 
                 if ((if_record_begin== true) && (if_record_end == false)){ // if you haven't reached the end of the record, continue
-                    console.log("here4"); 
                     console.log(line); 
                     current_record += line; 
                 }
                 
                 if ((line.toLowerCase()).localeCompare(end_keyword) == 0){
-                    console.log("here3"); 
                     current_record += line; 
-                    console.log("THE RECORD::: ", current_record); 
-                    records.push(current_record); 
+                    console.log("THE RECORD === \n", current_record); 
+
+                    records.push(current_record); // not adding to the array properly?
+                    
                     if_record_end = true; // the current record has ended 
+                    if_record_begin = false; 
                     current_record = ""; 
                 }
                 
