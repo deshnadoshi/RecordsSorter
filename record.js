e6bfcbea1a3fccd1cbc7b1991a39a926a78f4212
deshnadoshi <deshna.doshi@gmail.com>
2024-01-29 14:36:55 -0500

    error checking for file name

--- record.js
+++ record.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 
+let records = []; 
+
 fs.readFile('input.txt', (err, data) =>{
-    if (err) throw err; 
-    console.log(data.toString()); 
+    if (err){
+        console.log("Please rename your file to input.txt and re-run the program."); 
+    } else {
+        let file_data = data.toString(); 
+    } 
 }); 
